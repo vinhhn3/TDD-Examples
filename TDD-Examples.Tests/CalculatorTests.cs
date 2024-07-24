f2fa8c8db358b95b2ebdf6b270216340ad6fbf96
Vinh Hoang <vinhhn3@fpt.edu.vn>
2024-07-24 14:33:01 +0700

    Complete CalculatorTests

--- TDD-Examples.Tests/CalculatorTests.cs
+++ TDD-Examples.Tests/CalculatorTests.cs
@@ -1,5 +1,7 @@
 using NUnit.Framework;
 
+using System;
+
 namespace TDD_Examples.Tests
 {
   [TestFixture]
@@ -16,31 +18,74 @@ namespace TDD_Examples.Tests
     [Test]
     public void Add_ValidInputs_ReturnSum()
     {
+      // Assert
+      var a = 10;
+      var b = 20;
+      var expectedReuslt = 30;
 
+      // Act
+      var actualResult = calculator.Add(a, b);
+
+      // Assert
+      Assert.AreEqual(expectedReuslt, actualResult);
     }
 
     [Test]
     public void Substract_ValidInputs_ReturnSubstraction()
     {
+      // Arrange
+      var a = 20;
+      var b = 10;
+      var expectedReuslt = 10;
 
+      // Act
+      var actualResult = calculator.Subtract(a, b);
+
+      // Assert
+      Assert.AreEqual(expectedReuslt, actualResult);
     }
 
     [Test]
     public void Multiply_ValidInputs_ReturnMultiplication()
     {
+      // Arrange
+      var a = 20;
+      var b = 10;
+      var expectedReuslt = 200;
 
+      // Act
+      var actualResult = calculator.Multiply(a, b);
+
+      // Assert
+      Assert.AreEqual(expectedReuslt, actualResult);
     }
 
     [Test]
     public void Divide_ValidInput_ReturnDivision()
     {
+      // Arrange
+      var a = 10;
+      var b = 20;
+      var expectedReuslt = 0.5;
 
+      // Act
+      var actualResult = calculator.Divide(a, b);
+
+      // Assert
+      Assert.AreEqual(expectedReuslt, actualResult);
     }
 
     [Test]
     public void Divide_DenominatorEqualZero_ThrowExcetopion()
     {
+      // Arrange
+      var a = 10;
+      var b = 0;
 
+      // Act
+
+      // Assert
+      Assert.Throws<DivideByZeroException>(() => calculator.Divide(a, b));
     }
   }
 }
